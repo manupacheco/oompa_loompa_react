ba6d10e1aac0634791b7ed1c9d91b11ee3cbea6f
Manu Pacheco <mpachecopal@gmail.com>
2018-10-14 13:26:38 +0200

    add global style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import Navbar from './components/Navbar'
 import Home from './pages/Home';
 import Detail from './pages/Detail';
-import './App.css';
+
+const GlobalStyle = createGlobalStyle`
+  .container{
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Fragment>
+          <GlobalStyle/>
           <Navbar></Navbar>
           <Switch>
             <Route exact path='/' component={Home}/>
